3b5496e39625ac7989884eade8f815c8a1351ca1
yousifelhady <yousif.abdelhamed@gmail.com>
2015-10-19 03:45:22 +0200

    Update and rename Code-1.cpp to Programming Question(1).cpp

---
+++ Programming Question(1).cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class bankaccount
+{
+public:
+	bankaccount(string n , string accno, float b);
+	void create ();
+	void display ();
+	void desposite ();
+	void withdraw ();
+private:
+	string name;
+	string accountnumber;
+	float balance;
+};
+
+bankaccount::bankaccount(string n = "Name" , string accno = "Number" , float b = 0 )
+{
+		name = n ;
+		accountnumber = accno ;
+		balance = b ;
+		cout <<"Default name is: " << n << endl;
+		cout <<"Default account number is: " << accno << endl;
+		cout <<"Default balance is: " << b << endl;
+};
+void bankaccount::create ()
+{
+	    cout <<"Enter your name: " ;
+		cin >> name ;
+		cout << endl ;
+		cout <<"Enter your account number: " ;
+		cin >> accountnumber ;
+		cout << endl ;
+		cout <<"Enter your starting balance: " ;
+		cin >> balance ;
+		cout << endl ;
+};
+void bankaccount::desposite ()
+{
+	int deposite = 0 ;
+	cout <<"Enter your deposite value " ;
+	cin >> deposite ;
+	balance = balance + deposite ;
+};
+void bankaccount::withdraw ()
+{
+	int withdraw ;
+	cout <<"Enter your withdraw value " ;
+	cin >> withdraw ;
+	if ( withdraw > balance )
+		cout <<"Please enter a value bellow " << balance << endl;
+	else
+		balance = balance - withdraw ;
+	cout <<"Value after withdraw is "<< balance << endl ;
+};
+void bankaccount::display ()
+	{
+		cout<<endl;
+        cout<<setw(50)<<"Your Information is"<<endl;
+        cout<<setw(50)<<"Name: "<<name<<endl;
+        cout<<setw(50)<<"Account number: "<<accountnumber<<endl;
+        cout<<setw(50)<<"Balance: "<<balance<<endl;
+    }
+
+void main ()
+{
+	int x ;
+	bankaccount b1;
+	b1.create();
+	b1.display();
+	cout <<"If you want to deposite, Enter ""1"" & If you want to withdraw, Enter""2""" <<endl;
+	cin >> x;
+	if (x==1)
+	{b1.desposite();
+	b1.display();}
+	else if (x==2)
+	{b1.withdraw();
+	b1.display();}
+	else if (x!= 1 || 2)
+	{exit;}
+		
+	system ("pause");
+}
